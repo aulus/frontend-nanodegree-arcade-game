15240d206b26921ed38b979ef491bc5a326bdee7
Aulus Raffael <email@aulus.net>
2018-10-29 13:30:10 -0300

    fixed lowcase in engine

--- js/engine.js
+++ js/engine.js
@@ -178,7 +178,7 @@ var Engine = (function(global) {
         'images/enemy-bug.png',
         'images/char-boy.png',
         'images/gem-blue.png',
-        'images/Star.png',
+        'images/star.png',
     ]);
     Resources.onReady(init);
 
